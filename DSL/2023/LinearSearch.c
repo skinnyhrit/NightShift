ac5e6d653997911008bf18fc1c23558ad9e67745
tayadehritik <tayadehritik@gmail.com>
2023-04-19 22:06:24 +0530

    Linear Search done

---
+++ DSL/2023/LinearSearch.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdbool.h>
+bool exists(int arr[], int n, int searchNumber);
+
+int main()
+{
+    int arr[5] = {0,1,2,3,4};
+    int n = 5;
+    printf("Enter number to search \n");
+    int searchNumber;
+    scanf("%d",&searchNumber);
+    bool a = exists(arr,n,searchNumber);
+    printf("Exists: %s", a?"true":"false" );
+    return 0;
+}
+
+bool exists(int arr[], int n, int searchNumber)
+{
+   for(int i=0;i<n;i++)
+   {
+       if (arr[i] == searchNumber)
+       {
+            return true;
+       }
+   }
+   return false; 
+}
