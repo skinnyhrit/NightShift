10d65f626d859f73f85021624ed688aecdbbe187
tayadehritik <tayadehritik@gmail.com>
2023-05-05 00:00:17 +0530

    bubble sort done

---
+++ DSL/2023/BubbleSort.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+void BubbleSort(int*);
+
+int main()
+{
+    int arr[5] = {6,2,3,1,5};
+    BubbleSort(arr);    
+    return 0;
+}
+
+void BubbleSort(int* arr)
+{
+    
+    for(int i=5-1;i>=0;i--)
+    {
+       for(int j=0;j<i;j++)
+       {
+         if(arr[j] > arr[j+1])
+         {
+            int swap = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = swap;
+         }
+       }
+       for(int j=0;j<5;j++)
+       {
+            printf("%d\t",arr[j]);
+       }
+       printf("\n");
+    } 
+    
+}
