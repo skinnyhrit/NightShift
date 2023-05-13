26d338aab809c2f346f01a1fd97291c008e53a25
tayadehritik <braceritchie@protonmail.com>
2023-05-13 17:36:35 +0530

    Done with Queue in C

--- DSL/2023/Queue.c
+++ DSL/2023/Queue.c
@@ -7,50 +7,102 @@ struct Node {
 	struct Node* next;
 };
 
-void displayQueue(struct Node*, struct Node*);
+void displayQueue();
+void enqueue(int);
+int dequeue();
 
+struct Node* head = NULL;
+struct Node* tail = NULL;
 int main()
 {
 	int ch;
-	struct Node* head = NULL;
-	struct Node* tail = NULL;
+	int value;
 	while(1)
 	{
-		printf("Welcome to my Queue Program!!!\n");
+		printf("\nWelcome to my Queue Program!!!\n");
 		printf("Enter your choice\n");
 		printf("1.Display your Queue\n");
 		printf("2.Enqueue Operation\n");
 		printf("3.Dequeue Operation\n");
-		printf("4.Peek\n");
-		printf("5.Exit\n");
+		printf("4.Exit\n");
 		printf("Choice: ");
 		scanf("%d",&ch);
 		switch(ch)
 		{
-			case 1:	displayQueue(head, tail);
+			case 1:	displayQueue();
 				break;
-			case 2: printf("Performing Enqueue Operation\n");
+			case 2: printf("\nEnter value to enqueue\n");
+				scanf("%d",&value);	
+				enqueue(value);
 				break;
-			case 3: printf("Performing Dequeue Operation\n");
+			case 3: value = dequeue();
+				printf("\nDequeued value: %d\n",value);
 				break;
-			case 4: printf("Getting peak value\n");
-				break;
-			case 5: exit(0);
-				break;
-			default: printf("Incorrect choice\n");	
+			case 4: exit(0);
+
 		}
-
 	}
-	return 0;
 }
 
-
-void displayQueue(struct Node* head, struct Node* tail)
+void displayQueue()
 {
-	struct Node* ptr = head;
-	while(ptr != NULL)
+	if(head == NULL && tail == NULL)
 	{
-		printf("%d\t",ptr->value);
-		ptr = ptr->next;
+		printf("Queue is empty\n");
+	}
+	else
+	{
+		struct Node* ptr = head;
+		while(ptr != NULL)
+		{
+			printf("%d\t",ptr->value);
+			ptr = ptr->next;	
+		}	
 	}
 }
+
+void enqueue(int value)
+{
+	struct Node* temp;
+	temp = (struct Node*) malloc(sizeof(struct Node));
+	temp->value = value;
+	temp->next = NULL;
+
+	if(tail == NULL)
+	{
+		head = temp;
+		tail = head;	
+	}
+	else
+	{
+		tail->next = temp;
+		tail = temp;
+			
+	}
+
+}
+
+int dequeue()
+{
+	if(head == NULL)
+	{
+		printf("\nQueue is empty\n");
+		return 0;
+	}
+	else
+	{
+		int value = head->value;
+		struct Node* temp;
+		temp = head;
+		head = head->next;
+		free(temp);
+		if(head == NULL)
+		{
+			tail = NULL;
+		}
+		return value;		
+	}
+}
+
+
+
