e9daffa96db11f28aac5e485f713b05b6e5ae8fe
tayadehritik <tayadehritik@gmail.com>
2023-06-01 23:37:52 +0530

    still working on MazeSolver.c

--- DSL/2023/MazeSolver.c
+++ DSL/2023/MazeSolver.c
@@ -1,18 +1,61 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+struct Point{
+	int row;
+	int coloumn;
+};
+
+int TraverseMaze(struct Point,struct Point, struct Point, char, char[6][12], int[6][12]);
+
 int main()
 {
-	char arr[2][3] = {"xxx",
-			  "xx2"};
+	char arr[6][12] = {
+        			"xxxxxxxxxx x",
+        			"x        x x",
+        			"x        x x",
+        			"x xxxxxxxx x",
+        			"x          x",
+        			"x xxxxxxxxxx",
+			};
 
-	for(int i=0;i<2;i++)
+	int visitedArr[6][12];
+
+	for(int i=0;i<6;i++)
 	{
-		for(int j=0;j<3;j++)
+		for(int j=0;j<12;j++)
 		{
-			printf("%c",arr[i][j]);
+			visitedArr[i][j] = 0;
 		}
-		printf("\n");
 	}
+
+	int rows = 6;
+	int columns = 12;
+	struct Point start = {5,1};
+	struct Point end = {0,10};
+	struct Point curr = {4,1};
+	char wall = '#';
+
+	TraverseMaze(start,end,curr,wall,arr);	
+	return 0;
+}
+
+int TraverseMaze(struct Point start, struct Point end, struct Point curr, char wall, char arr[6][12], int vistedArr[6][12])
+{
+	if((curr.row == end.row && curr.coloumn == end.coloumn))
+	{
+		return 1;
+	}
+
+	if(arr[curr.row][curr.coloumn] == wall)
+	{
+		return 0;
+	}	
+
+	if(visitedArr[curr.row][curr.coloumn] == 1)
+	{
+		return 0;
+	}	
+		
 	return 0;
 }
