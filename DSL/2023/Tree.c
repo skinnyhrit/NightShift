00347d6c46f2123519d9c52269ea7f097b14b359
tayadehritik <tayadehritik@gmail.com>
2023-09-18 21:09:18 +0530

    breadth first traversal done

--- DSL/2023/Tree.c
+++ DSL/2023/Tree.c
@@ -9,14 +9,32 @@ struct Node
 	struct Node* right;
 };
 
+struct QNode
+{
+	struct Node* data;
+	struct QNode* next;
+
+};
+
+
+
+void enqueue(struct Node*);
+struct Node* dequeue();
+struct QNode* head = NULL;
+struct QNode* tail = NULL;
+void displayQueue();
+
 struct Node* listToBinaryTree(struct Node*,int idx, int[]);
 void printInOrder(struct Node*);
 void printPreOrder(struct Node*);
 void printPostOrder(struct Node*);
+void printBreadthFirst(struct Node*);
 struct Node* root;
 
 int main()
 {
+	
+
 	int arr[] = {1, 2, 3, 4, 5};	
 	root = listToBinaryTree(root, 0, arr);
 	printf("In order : ");
@@ -24,7 +42,9 @@ int main()
 	printf("\nPre order : ");
 	printPreOrder(root);
 	printf("\nPost order : ");
-	printPostOrder(root);	
+	printPostOrder(root);
+	printf("\nBreadth first : ");
+	printBreadthFirst(root);	
 	return 0;
 
 }
@@ -79,3 +99,77 @@ void printPostOrder(struct Node* temp)
 		printf("%d ",temp->data);
 	}
 }
+
+void printBreadthFirst(struct Node* temp)
+{
+
+	enqueue(temp);
+	struct Node* trav = dequeue();	
+
+	while(trav != NULL)
+	{
+		enqueue(trav->left);
+		enqueue(trav->right);
+		printf("%d ",trav->data);
+		trav = dequeue();	
+	}
+			
+}
+
+void enqueue(struct Node* data)
+{
+	struct QNode* temp = (struct QNode*) malloc(sizeof(struct QNode));
+	temp->data = data;
+	temp->next = NULL;	
+	if(head == NULL && tail == NULL)
+	{
+		tail = temp;
+		head = tail;
+	}
+	else
+	{
+		tail->next = temp;
+		tail = temp;	
+	}
+}
+
+struct Node* dequeue()
+{
+	if(head == NULL && tail == NULL)
+	{
+		return NULL;
+	}
+	else
+	{
+		struct Node* data = head->data;
+		struct QNode* temp = head;
+		if(head == tail)
+		{
+			tail = NULL;	
+		}
+		head = head->next;
+		free(temp);
+		return data;
+	}
+	
+}
+
+void displayQueue()
+{
+	if(head == NULL)
+	{
+		printf("Queue is empty\n");
+	}
+	else
+	{
+		struct QNode* trav = head;
+		while(trav != NULL)
+		{
+			printf("%d ",trav->data->data);
+			trav = trav->next;
+		}
+		printf("\n");
+	}
+}
+
+
